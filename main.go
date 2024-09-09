41e7f959940815de670bbe74a02062f93ddaf183
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-09 11:01:56 -0300

    if/else

--- main.go
+++ main.go
@@ -3,23 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
-	var user user = user{
-		name: "Rafinha",
-		age:  18,
+	if 5 > 2 {
+		fmt.Print("Sim!")
+	} else {
+		fmt.Print("Não!")
 	}
-
-	fmt.Print(user)
-}
-
-//Pode ser instanciado fora do arquivo
-type User struct {
-	Email    string //pode ser visto quando instanciar
-	password string // não pode ser visto ao instanciar
-}
-
-//Não pode ser instanciado fora do arquivo
-type user struct {
-	name string
-	age  int
 }
