c4bbcef91560da0a7ec4a930762496d19bacc0d4
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-09 11:25:45 -0300

    fallthrough/break

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	test := "test"
+	test := "teste"
 
 	switch test {
 
 	case "teste", "teste3":
 		fmt.Print("Primeira condição!")
-
+		fallthrough //faz ele cair para o case de baixo tambem
 	case "test":
 		fmt.Print("Segunda condição!")
+		fallthrough //continua caindo
+	
+	case "test5":
+		fmt.Print("Terceira condição!")
+	break // faz ele parar de cair
+
+	case "teste6":
+		fmt.Print("Teste")
 	}
 }
