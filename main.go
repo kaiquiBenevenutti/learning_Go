effe054edc70e6c2d98111a8cf068d530d57a09b
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2024-09-11 08:57:31 -0300

    for

--- main.go
+++ main.go
@@ -3,25 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	test := "teste7"
 
-	switch test {
-
-	case "teste", "teste3":
-		fmt.Print("Primeira condição!")
-		fallthrough //faz ele cair para o case de baixo tambem
-	case "test":
-		fmt.Print("Segunda condição!")
-		fallthrough //continua caindo
-	
-	case "test5":
-		fmt.Print("Terceira condição!")
-		break // faz ele parar de cair
-
-	case "teste6":
-		fmt.Print("Teste")
-
-	default:
-		fmt.Print("Default")
+    for i := 0; i <= 10; i++ {
+		fmt.Println(i)
 	}
 }
